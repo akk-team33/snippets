098345fb16e62781174c2dda06e6217552b20863
andi <akk-pg@team33.de>
2022-11-19 11:14:08 +0100

    some refinement

--- src/main/java/de/team33/snippets/e1/runtime/User.java
+++ src/main/java/de/team33/snippets/e1/runtime/User.java
@@ -5,12 +5,15 @@ import de.team33.patterns.lazy.e1.Lazy;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Provides user related information from the runtime environment.
  */
 public class User {
 
+    private static final String DOT = ".";
     private static final String HOME_KEY = "user.home";
     private static final Lazy<Path> HOME = new Lazy<>(() -> Optional.ofNullable(System.getProperty(HOME_KEY))
                                                                     .map(Paths::get)
@@ -26,33 +29,35 @@ public class User {
     }
 
     /**
-     * Returns a {@link Path} intended to refer a directory for storing configuration data.
-     * The directory name results from the name of a given java package and starts with a period.
+     * Returns a {@link Path} intended to refer a user related directory for storing configuration data.
+     * The directory name results from the name of a given java package.
      * <p>
-     * It is not checked whether the resulting path actually refers a directory or another file system object.
+     * It is not checked whether the resulting path actually refers a directory or even exists.
      */
     public static Path configPath(final Package p) {
-        return home().resolve("." + p.getName());
+        return home().resolve(".etc").resolve(p.getName());
     }
 
     /**
-     * Returns a {@link Path} intended to refer a regular file for storing configuration data.
+     * Returns a {@link Path} intended to refer a user related regular file for storing configuration data.
      * The file name results from the name of a given java class.
      * <p>
-     * It is not checked whether the resulting path actually refers a directory or another file system object.
+     * It is not checked whether the resulting path actually refers a regular file or even exists.
      */
-    public static Path configFile(final Class<?> c) {
+    public static Path configFile(final Class<?> c, final String... extensions) {
         final Package p = c.getPackage();
-        final String name = c.getCanonicalName().substring(p.getName().length() + 1);
+        final String name = c.getCanonicalName()
+                             .substring(p.getName().length() + 1) + normal(extensions);
         return configPath(p).resolve(name);
     }
 
-    private static Path newHome() {
-        return Optional.ofNullable(System.getProperty(HOME_KEY))
-                       .map(Paths::get)
-                       .orElseThrow(User::missingHomeException)
-                       .toAbsolutePath()
-                       .normalize();
+    private static String normal(final String... extensions) {
+        final String result = Stream.of(extensions)
+                                    .map(entry -> entry.split("[\\s.]+"))
+                                    .flatMap(Stream::of)
+                                    .filter(entry -> !"".equals(entry))
+                                    .collect(Collectors.joining("."));
+        return "".equals(result) ? result : ("." + result);
     }
 
     private static IllegalStateException missingHomeException() {
--- src/test/java/de/team33/test/snippets/e1/runtime/UserTest.java
+++ src/test/java/de/team33/test/snippets/e1/runtime/UserTest.java
@@ -5,14 +5,21 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static java.lang.String.format;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UserTest {
 
@@ -58,27 +65,49 @@ class UserTest {
 
     @Test
     final void configPath_existingFile() throws IOException {
+        Files.createDirectories(Expected.CONFIG_PATH.getParent());
         Files.createFile(Expected.CONFIG_PATH);
         final Path result = User.configPath(CLASS.getPackage());
         assertEquals(Expected.CONFIG_PATH, result);
     }
 
-    @Test
-    final void configFile() {
-        final Path result = User.configFile(CLASS);
-        assertEquals(Expected.OUTER_PATH, result);
+    @ParameterizedTest
+    @EnumSource(Extension.class)
+    final void configFile(final Extension extension) {
+        final Path result = User.configFile(CLASS, extension.input);
+        final Path expected = Paths.get(Expected.OUTER_PATH + extension.expected);
+        assertEquals(expected, result);
     }
 
-    @Test
-    final void configFile_SubClass() {
-        final Path result = User.configFile(Expected.class);
-        assertEquals(Expected.INNER_PATH, result);
+    @ParameterizedTest
+    @EnumSource(Extension.class)
+    final void configFile_SubClass(final Extension extension) {
+        final Path result = User.configFile(Expected.class, extension.input);
+        final Path expected = Paths.get(Expected.INNER_PATH + extension.expected);
+        assertEquals(expected, result);
+    }
+
+    enum Extension {
+        EXT0(""),
+        EXT1("", ""),
+        EXT2("", "", "  ", "\t", "\n"),
+        EXT3(".json", ".json"),
+        EXT4(".cnf", "cnf"),
+        EXT5(".abc.def.g.h.i.j.k.l.*", "abc", " ..def", "g h i", " j.k.l ", " * ");
+
+        private final String expected;
+        private final String[] input;
+
+        Extension(final String expected, final String... input) {
+            this.expected = expected;
+            this.input = input;
+        }
     }
 
     private static class Expected {
 
         private static final Path HOME = Paths.get(System.getProperty("user.home"));
-        private static final Path CONFIG_PATH = HOME.resolve("." + CLASS.getPackage().getName());
+        private static final Path CONFIG_PATH = HOME.resolve(".etc").resolve(CLASS.getPackage().getName());
         private static final Path OUTER_PATH = CONFIG_PATH.resolve(CLASS.getSimpleName());
         private static final Path INNER_PATH = CONFIG_PATH.resolve(CLASS.getSimpleName()
                                                                            + "."
