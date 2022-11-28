287dcd0f75e9e1d88574c488c4341278a44874e3
Kluge-Kaindl, Andreas <andreas.klugekaindl@governikus.de>
2022-11-28 09:04:36 +0100

    new: generation: array

---
+++ src/main/java/de/team33/snippets/e1/creation/Array.java
@@ -0,0 +1,20 @@
+package de.team33.snippets.e1.creation;
+
+public class Array {
+
+    public static int[] of(final int... elements) {
+        return elements;
+    }
+
+    public static long[] of(final long... elements) {
+        return elements;
+    }
+
+    public static double[] of(final double... elements) {
+        return elements;
+    }
+
+    public static <E> E[] of(final E... elements) {
+        return elements;
+    }
+}
